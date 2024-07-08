303f8a27b084cd3978d8214e284796f969445cfd
Joel Wurtz <jwurtz@jolicode.com>
2024-07-08 09:24:09 +0200

    chore(features): split code with more feature, so pure lib have less dependencies by default

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,10 @@
 pub mod client_hints;
 pub mod device_detector;
+#[cfg(feature = "build-binary")]
 pub mod http;
 pub mod known_browsers;
 pub mod known_oss;
 pub mod parsers;
 
-// cfg(feature = "ffi")
+#[cfg(feature = "ffi")]
 pub mod ffi;
